fe1f6ca6ca0e805e1d611e8684edb38d8ac63d80
Xingyu Luo <xingyu.orchard@gmail.com>
2025-10-18 21:07:22 -0700

    feat(about): add About / Info page and navigation

--- app/src/main/java/edu/sjsu/android/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/edu/sjsu/android/myapplication/ui/home/HomeFragment.java
@@ -4,28 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import edu.sjsu.android.myapplication.R;
 import edu.sjsu.android.myapplication.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // Use ViewBinding (your existing code)
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Update text using your ViewModel (existing feature)
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Add the About button navigation here
+        Button aboutBtn = root.findViewById(R.id.btn_go_to_about);
+        aboutBtn.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            navController.navigate(R.id.aboutFragment);
+        });
+
         return root;
     }
 
