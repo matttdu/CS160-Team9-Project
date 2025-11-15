2b007311f405fb7f5e5474cdf2b52a1fe8ef2f6d
Hardeep Kainth <55467013+hdkainth@users.noreply.github.com>
2025-11-14 20:46:57 -0800

    Customized the profile username

--- app/src/main/java/edu/sjsu/android/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/sjsu/android/myapplication/ui/notifications/NotificationsFragment.java
@@ -1,16 +1,23 @@
 package edu.sjsu.android.myapplication.ui.notifications;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import edu.sjsu.android.myapplication.databinding.FragmentNotificationsBinding;
+
+import edu.sjsu.android.myapplication.R;
 
 public class NotificationsFragment extends Fragment {
 
@@ -19,13 +26,18 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_notifications, container, false);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        String username = requireContext()
+                .getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+                .getString("loggedInUser", "");
 
-        return root;
+
+        TextView usernameField = v.findViewById(R.id.profile_header);
+        usernameField.setText("Welcome, " + username + "!");
+
+
+        return v;
     }
 
     @Override
